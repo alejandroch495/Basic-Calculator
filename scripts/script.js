eaeb9265367732b2c86422fa73c1244a414f93cd
Asa White <whiteasa94@gmail.com>
2020-11-23 11:32:30 -0800

    adds functionality to the number buttons, and fixes up some of the html

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 class Calculator {
     constructor(previous, current) {
-        this.previous = previous
-        this.current = current
+        this.previous = previous;
+        this.current = current;
+        this.clear();
     }
     clear() {
         this.currentOperand = "";
@@ -13,7 +14,10 @@ class Calculator {
 
     }
     appendNum(number) {
-        this.currentOperand = number
+        if(number==='.' && this.currentOperand.includes('.')){
+            return
+        }
+        this.currentOperand = this.currentOperand.toString() + number.toString();
     }
     operationSelected(operation) {
 
@@ -39,13 +43,13 @@ output = document.getElementById('output');
 
 const numButtons = document.querySelectorAll('[data-numbers]');
 const opsButtons = document.querySelectorAll('[data-operators]');
-const equalButton = document.querySelector('[data-equal]')
-const delButton = document.querySelector('[data-delete]')
-const acButton = document.querySelector('[data-ac')
-const previous = document.querySelector('[data-previous]')
-const current = document.querySelector('[data-current]')
+const equalButton = document.querySelector('[data-equal]');
+const delButton = document.querySelector('[data-delete]');
+const acButton = document.querySelector('[data-ac');
+const previous = document.querySelector('[data-previous]');
+const current = document.querySelector('[data-current]');
 
-const calculator = new Calculator(previous, current)
+const calculator = new Calculator(previous, current);
 
 numButtons.forEach(button => {
     button.addEventListener('click', () => {

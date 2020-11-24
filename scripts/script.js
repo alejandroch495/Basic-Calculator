17e6f96cf54c73c002718ed9a46ae97d24a3fa19
Asa White <whiteasa94@gmail.com>
2020-11-23 19:29:48 -0800

    added full calc functionality. Still needs negative button and bug fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
+const numButtons = document.querySelectorAll('[data-numbers]');
+const opsButtons = document.querySelectorAll('[data-operators]');
+const equalButton = document.querySelector('[data-equal]');
+const delButton = document.querySelector('[data-delete]');
+const acButton = document.querySelector('[data-ac');
+const previous = document.querySelector('[data-previous]');
+const current = document.querySelector('[data-current]');
+
 class Calculator {
     constructor(previous, current) {
         this.previous = previous;
@@ -20,34 +28,49 @@ class Calculator {
         this.currentOperand = this.currentOperand.toString() + number.toString();
     }
     operationSelected(operation) {
-
+        if(this.currentOperand===""){
+            return
+        }
+        if(this.previousOperand !==''){
+            this.calc();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
     }
     calc() {
-
+        let equals = '';
+        let previous = parseFloat(this.previousOperand);
+        let current = parseFloat(this.currentOperand);
+        if(isNaN(current) || isNaN(previous)){
+            return
+        }
+        switch(this.operation){
+            case '+':
+                equals = previous + current;
+                break;
+            case '-':
+                equals = previous - current;
+                break;
+            case '*':
+                equals = previous * current;
+                break;
+            case '/':
+                equals = previous / current;
+                break;
+            default:
+                return;
+        }
+        this.currentOperand = equals;
+        this.operation = undefined;
+        this.previousOperand = "";
     }
 
     updateDisplay() {
-        this.current.innerText = this.currentOperand
+        this.current.innerText = this.currentOperand;
+        this.previous.innerText = this.previousOperand;
     }
 }
-
-
-
-
-var printer = [];
-var order = [];
-var x = [];
-input = document.getElementById('input');
-var output = [];
-output = document.getElementById('output');
-
-const numButtons = document.querySelectorAll('[data-numbers]');
-const opsButtons = document.querySelectorAll('[data-operators]');
-const equalButton = document.querySelector('[data-equal]');
-const delButton = document.querySelector('[data-delete]');
-const acButton = document.querySelector('[data-ac');
-const previous = document.querySelector('[data-previous]');
-const current = document.querySelector('[data-current]');
 
 const calculator = new Calculator(previous, current);
 
@@ -60,5 +83,22 @@ numButtons.forEach(button => {
 
 delButton.addEventListener('click', ()=>{
     calculator.delete();
+    calculator.updateDisplay();
+})
+
+opsButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.operationSelected(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+equalButton.addEventListener('click', ()=>{
+    calculator.calc();
+    calculator.updateDisplay();
+})
+
+acButton.addEventListener('click', ()=>{
+    calculator.clear();
     calculator.updateDisplay();
 })
